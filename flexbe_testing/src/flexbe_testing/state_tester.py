1a8e47e4c1e9ce39c9199dbe6d0c48f72b4362af
Philipp Schillinger <philsplus@gmail.com>
2015-11-21 10:31:33 +0100

    [flexbe_testing] Added import_only option for tests

--- flexbe_testing/src/flexbe_testing/state_tester.py
+++ flexbe_testing/src/flexbe_testing/state_tester.py
@@ -41,27 +41,10 @@ class StateTester(object):
 
 		if not self._compact_format: print ''
 		self._counter += 1
-		print '\033[34;1m#%2d %s \033[0m\033[34m(%s > %s)\033[0m' % (self._counter, name, config['class'], config['outcome'])
 
+		import_only = config.get('import_only', False)
+		print '\033[34;1m#%2d %s \033[0m\033[34m(%s%s)\033[0m' % (self._counter, name, config['class'], ' > %s' % config['outcome'] if not import_only else '')
 		prefix = '>>>' if not self._compact_format else '  >'
-
-		# import state
-		state = None
-		try:
-			package = __import__(config['path'], fromlist=[config['path']])
-			clsmembers = inspect.getmembers(package, lambda member: inspect.isclass(member) and member.__module__ == package.__name__)
-			StateClass = next(c for n,c in clsmembers if n == config['class'])
-			if config.has_key('params'):
-				for key, value in config['params'].iteritems():
-					if isinstance(value, basestring) and value.startswith('lambda '):
-						config['params'][key] = eval(value)
-				state = StateClass(**config['params'])
-			else:
-				state = StateClass()
-			if self._print_debug_positive: print '\033[1m  +\033[0m state imported'
-		except Exception as e:
-			print '\033[31;1m%s\033[0m\033[31m unable to import state %s (%s):\n\t%s\033[0m' % (prefix, config['class'], config['path'], str(e))
-			return 0
 
 		# prepare rosbag if available
 		bag = None
@@ -78,58 +61,82 @@ class StateTester(object):
 			bag = rosbag.Bag(bagpath)
 			if self._print_debug_positive: print '\033[1m  +\033[0m using data source: %s' % bagpath
 
-		# set input values
-		userdata = smach.UserData()
-		if config.has_key('input'):
-			for input_key, input_value in config['input'].iteritems():
-				if isinstance(input_value, basestring) and len(input_value) > 1 and input_value[0] == '/' and input_value[1] != '/' and bag is not None:
-					(_, input_value, _) = list(bag.read_messages(topics=[input_value]))[0]
-				elif isinstance(input_value, basestring) and input_value.startswith('lambda '):
-					input_value = eval(input_value)
-				elif input_value == 'None':
-					input_value = None
-				elif isinstance(input_value, basestring) and len(input_value) > 1 and input_value[0] == '/' and input_value[1] == '/':
-					input_value = input_value[1:]
-				userdata[input_key] = input_value
+		# import state
+		state = None
+		try:
+			package = __import__(config['path'], fromlist=[config['path']])
+			clsmembers = inspect.getmembers(package, lambda member: inspect.isclass(member) and member.__module__ == package.__name__)
+			StateClass = next(c for n,c in clsmembers if n == config['class'])
+			if config.has_key('params'):
+				for key, value in config['params'].iteritems():
+					config['params'][key] = self._parse_data_value(value, bag)
+				state = StateClass(**config['params'])
+			elif not import_only:
+				state = StateClass()
+			if self._print_debug_positive: print '\033[1m  +\033[0m state imported'
+		except Exception as e:
+			print '\033[31;1m%s\033[0m\033[31m unable to import state %s (%s):\n\t%s\033[0m' % (prefix, config['class'], config['path'], str(e))
+			return 0
 
-		# set output values
-		expected = dict()
-		if config.has_key('output'):
-			for output_key, output_value in config['output'].iteritems():
-				if isinstance(output_value, basestring) and len(output_value) > 1 and output_value[0] == '/' and output_value[1] != '/' and bag is not None:
-					(_, output_value, _) = bag.read_messages(topics=[output_value])[0]
-				elif output_value == 'None':
-					output_value = None
-				elif isinstance(output_value, basestring) and len(output_value) > 1 and output_value[0] == '/' and output_value[1] == '/':
-					output_value = output_value[1:]
-				expected[output_key] = output_value
+		if not import_only:
+			# set input values
+			userdata = smach.UserData()
+			if config.has_key('input'):
+				for input_key, input_value in config['input'].iteritems():
+					userdata[input_key] = self._parse_data_value(input_value, bag)
 
-		# execute state
-		outcome = LoopbackState._loopback_name
-		while outcome == LoopbackState._loopback_name and not rospy.is_shutdown():
-			outcome = state.execute(userdata)
+			# set output values
+			expected = dict()
+			if config.has_key('output'):
+				for output_key, output_value in config['output'].iteritems():
+					expected[output_key] = self._parse_data_value(output_value, bag)
 
-		# evaluate output
-		output_ok = True
-		for expected_key, expected_value in expected.iteritems():
-			if expected_key in userdata.keys():
-				if expected_value != userdata[expected_key]:
-					if self._print_debug_negative: print '\033[1m  -\033[0m wrong result for %s: %s != %s' % (expected_key, userdata[expected_key], expected_value)
-					output_ok = False
-		if len(expected) > 0 and output_ok:
-			if self._print_debug_positive: print '\033[1m  +\033[0m all result outputs match expected'
+			# execute state
+			outcome = LoopbackState._loopback_name
+			while outcome == LoopbackState._loopback_name and not rospy.is_shutdown():
+				outcome = state.execute(userdata)
 
-		# evaluate outcome
-		outcome_ok = outcome == config['outcome']
-		if outcome_ok:
-			if self._print_debug_positive: print '\033[1m  +\033[0m correctly returned outcome %s' % outcome
-		else:
-			if self._print_debug_negative: print '\033[1m  -\033[0m wrong outcome: %s' % outcome
+			# evaluate output
+			output_ok = True
+			for expected_key, expected_value in expected.iteritems():
+				if expected_key in userdata.keys():
+					if expected_value != userdata[expected_key]:
+						if self._print_debug_negative: print '\033[1m  -\033[0m wrong result for %s: %s != %s' % (expected_key, userdata[expected_key], expected_value)
+						output_ok = False
+			if len(expected) > 0 and output_ok:
+				if self._print_debug_positive: print '\033[1m  +\033[0m all result outputs match expected'
+
+			# evaluate outcome
+			outcome_ok = outcome == config['outcome']
+			if outcome_ok:
+				if self._print_debug_positive: print '\033[1m  +\033[0m correctly returned outcome %s' % outcome
+			else:
+				if self._print_debug_negative: print '\033[1m  -\033[0m wrong outcome: %s' % outcome
 
 		# report result
-		if outcome_ok and output_ok:
+		if import_only or outcome_ok and output_ok:
 			print '\033[32;1m%s\033[0m\033[32m %s completed!\033[0m' % (prefix, name)
 			return 1
 		else:
 			print '\033[31;1m%s\033[0m\033[31m %s failed!\033[0m' % (prefix, name)
 			return 0
+
+
+	def _parse_data_value(self, data_value, bag):
+		# message data
+		if isinstance(data_value, basestring) and len(data_value) > 1 and data_value[0] == '/' and data_value[1] != '/' and bag is not None:
+			(_, data_value, _) = list(bag.read_messages(topics=[data_value]))[0]
+
+		# anonymous function
+		elif isinstance(data_value, basestring) and data_value.startswith('lambda '):
+			data_value = eval(data_value)
+
+		# None
+		elif data_value == 'None':
+			data_value = None
+
+		# escaped backslash at the beginning
+		elif isinstance(data_value, basestring) and len(data_value) > 1 and data_value[0] == '/' and data_value[1] == '/':
+			data_value = data_value[1:]
+
+		return data_value
