89a2f9fb7883db9fe8c866c4d923da830e74c24a
Philipp Schillinger <philsplus@gmail.com>
2018-02-25 18:52:50 +0100

    [flexbe_testing] Correct states for selftest (fix #49)

--- flexbe_testing/src/flexbe_testing/test/test_sub_state.py
+++ flexbe_testing/src/flexbe_testing/test/test_sub_state.py
@@ -10,7 +10,7 @@ class TestSubState(EventState):
 	def __init__(self, topic):
 		'''Constructor'''
 		super(TestSubState, self).__init__(outcomes=['received', 'unavailable'],
-										output_keys=['result'])
+										output_keys=['output_value'])
 		self._topic = topic
 		self._sub = ProxySubscriberCached({self._topic: String})
 		self._msg_counter = 0
@@ -19,12 +19,13 @@ class TestSubState(EventState):
 		
 	def execute(self, userdata):
 		if self._msg_counter == 0 and rospy.Time.now() > self._timeout:
+			userdata.output_value = None
 			return 'unavailable'
 
 		if self._sub.has_msg(self._topic):
 			msg = self._sub.get_last_msg(self._topic)
 			self._sub.remove_last_msg(self._topic)
-			userdata.result = msg.data
+			userdata.output_value = msg.data
 			self._msg_counter += 1
 
 		if self._msg_counter == 3:
